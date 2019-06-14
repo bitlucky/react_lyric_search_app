3d26f4de6575ea0119df49ed34e9184fbb77ccb8
bitlucky <lokeshtiwari877@gmail.com>
2019-06-14 12:09:05 +0530

    Context Api Route added

---
+++ src/components/layout/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tracks from '../tracks/Tracks'
+const Index = () => {
+    return (
+        <div>
+            <Tracks />
+        </div>
+    );
+}
+
+export default Index;
